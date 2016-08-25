560604d3b42742a20da1ed38e59d53dfd1787580
ade ari <ade_ari_w0@yahoo.com>
2016-08-25 12:13:48 +0700

    dfdff

--- app/src/main/java/com/example/widianta/beritasurabaya/PropertiesData.java
+++ app/src/main/java/com/example/widianta/beritasurabaya/PropertiesData.java
@@ -7,6 +7,6 @@ public class PropertiesData {
 //    public final static String domain = "http://192.168.0.102/";
     public final static String domain = "http://sby.akses.top/";
     public final static String ftpdomain = "dsfdewewef";
-    public final static String ftpusername = "sderer45fdff";
-    public final static String ftppassword = "cxbfgfg";
+    public final static String ftpusername = "sdererdc45fdff";
+    public final static String ftppassword = "cxbfgvcvcvfg";
 }
